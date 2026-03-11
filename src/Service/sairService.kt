0a4896a0f7f998104e3693335c329bafb802062e
Gabryel Rodrigues <gabryelsantos346@gmail.com>
2026-03-10 22:12:02 -0300

    Documentando código e melhorando a experiência de saida

--- src/Service/sairService.kt
+++ src/Service/sairService.kt
@@ -5,17 +5,20 @@ import kotlin.system.exitProcess
 
 class SairService {
 
-    val inicioService = InicioService()
+    val inicioService = InicioService()     //Instanciando a Classe inicioService
 
-    fun sair(cliente: Cliente) {
+    fun sair(cliente: Cliente) {        //  Função de sair
 
-        println("Você deseja sair? S/N")
+        println("Você deseja sair? S/N")            //Confirmação de saida
         val escolha = readln().uppercase()
 
         when (escolha) {
 
-            "S" -> exitProcess(0)
-            "N" -> inicioService.menu(cliente)
+            "S" ->{
+                println("Saindo...")
+                exitProcess(0)
+            }
+            "N" -> inicioService.menu(cliente)              //Ações de voltação de saida ou permanencia do usuario
 
             else -> {
                 println("Opção inválida.")
